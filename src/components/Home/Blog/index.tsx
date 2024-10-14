47464221e6241f82960fee6708f78b1d3201b57d
gzyxds <348310633@qq.com>
2024-10-15 05:46:46 +0800

    初映Matrix是为短视频创作者提供IP授权、短剧分发、游戏分发、短视频cps、短剧分销平台、短剧分销cps、短剧推广授权等多渠道多平台的变现项目

--- src/components/Home/Blog/index.tsx
+++ src/components/Home/Blog/index.tsx
@@ -6,32 +6,26 @@ import { Blog } from "@/types/blog";
 const posts: Blog[] = [
   {
     _id: 1,
-    title:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum dictum euismod",
-    metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum dictum euismod malesuada lacus, non consequat quam.",
+    title: "如何打造爆款短剧素材",
+    metadata: "本文探讨了最新的技术趋势和创新，以及它们如何影响我们的日常生活和工作方式。",
     mainImage: "/images/blog/blog-1.png",
-    author: "John Doe",
+    author: "张三",
     publishedAt: "2022-01-01",
   },
   {
     _id: 2,
-    title:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum dictum euismod",
-    metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum dictum euismod malesuada lacus, non consequat quam.",
+    title: "短剧CPS问题答疑及系统使用指南",
+    metadata: "本文分析了人工智能在医疗诊断、药物研发等方面的应用，以及面临的伦理和技术挑战。",
     mainImage: "/images/blog/blog-2.png",
-    author: "John Doe",
+    author: "李四",
     publishedAt: "2022-01-01",
   },
   {
     _id: 3,
-    title:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum dictum euismod",
-    metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum dictum euismod malesuada lacus, non consequat quam.",
+    title: "成为我们的短剧代理，共创精彩人生！",
+    metadata: "本文讨论了企业在实现可持续发展目标中的角色，以及如何将环保理念融入商业策略。",
     mainImage: "/images/blog/blog-3.png",
-    author: "John Doe",
+    author: "王五",
     publishedAt: "2022-01-01",
   },
 ];
@@ -44,8 +38,8 @@ const Blog = async () => {
     >
       {/* <!-- section title --> */}
       <SectionHeader
-        title={"Latest Blogs & News"}
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent condimentum dictum euismod malesuada lacus, non consequat quam."
+        title={"最新博客与新闻"}
+        description="我们为您带来最新的行业动态、技术趋势和深度分析，助您洞察未来，把握机遇。"
       />
 
       <div className="mx-auto w-full max-w-[1170px] px-4 sm:px-8 xl:px-0">
@@ -56,7 +50,7 @@ const Blog = async () => {
               ?.slice(0, 3)
               .map((item, key: number) => <BlogItem blog={item} key={key} />)
           ) : (
-            <p>No posts found</p>
+            <p>暂无文章</p>
           )}
         </div>
       </div>
