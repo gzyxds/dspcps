46ea975cf3174265112c2a6011bd73d7f7d0fc21
枭格科技 <348310633@qq.com>
2024-11-07 22:13:00 +0800

    cps
    cps

--- src/components/Home/Hero/brandData.tsx
+++ src/components/Home/Hero/brandData.tsx
@@ -2,7 +2,7 @@
  * @Author: gzyxds 348310633@qq.com
  * @Date: 2024-10-15 14:50:41
  * @LastEditors: 枭格科技 348310633@qq.com
- * @LastEditTime: 2024-11-07 22:09:54
+ * @LastEditTime: 2024-11-07 22:12:57
  * @FilePath: \dspcps\src\components\Home\Hero\brandData.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -21,7 +21,7 @@ const brandData: Brand[] = [
 		name: "NextJSTemplates",
 		link: "https://play.urlnet.cn",
 		image: (
-			<img src="/images/icon/抖音.svg" alt="NextJSTemplates" width="101" height="28" />
+			<img src="/images/icon/dy.svg" alt="NextJSTemplates" width="101" height="28" />
 		),
 	},
 	{
