db52ae08c7509a907d2dd33c9c38255cbf6a576d
枭格科技 <348310633@qq.com>
2024-11-07 22:15:34 +0800

    cps
    cps

--- src/components/Home/Hero/brandData.tsx
+++ src/components/Home/Hero/brandData.tsx
@@ -2,7 +2,7 @@
  * @Author: gzyxds 348310633@qq.com
  * @Date: 2024-10-15 14:50:41
  * @LastEditors: 枭格科技 348310633@qq.com
- * @LastEditTime: 2024-11-07 22:12:57
+ * @LastEditTime: 2024-11-07 22:15:29
  * @FilePath: \dspcps\src\components\Home\Hero\brandData.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -29,7 +29,7 @@ const brandData: Brand[] = [
 		name: "FormBold",
 		link: "https://play.urlnet.cn",
 		image: (
-			<img src="/images/icon/快手.svg" alt="FormBold" width="153" height="28" />
+			<img src="/images/icon/ks.svg" alt="FormBold" width="153" height="28" />
 		),
 	},
 	{
@@ -37,7 +37,7 @@ const brandData: Brand[] = [
 		name: "Lineicons",
 		link: "https://play.urlnet.cn",
 		image: (
-			<img src="/images/icon/视频号.svg" alt="Lineicons" width="173" height="28" />
+			<img src="/images/icon/sph.svg" alt="Lineicons" width="173" height="28" />
 		),
 	},
 	{
@@ -45,7 +45,7 @@ const brandData: Brand[] = [
 		name: "Uideck",
 		link: "https://play.urlnet.cn",
 		image: (
-			<img src="/images/icon/小红书.svg" alt="Uideck" width="119" height="28" />
+			<img src="/images/icon/xhs.svg" alt="Uideck" width="119" height="28" />
 		),
 	},
 	{
@@ -53,7 +53,7 @@ const brandData: Brand[] = [
 		name: "Graygrids",
 		link: "https://play.urlnet.cn",
 		image: (
-			<img src="/images/icon/企业微信.svg" alt="Graygrids" width="134" height="28" />
+			<img src="/images/icon/qywx.svg" alt="Graygrids" width="134" height="28" />
 		),
 	},
 	{
