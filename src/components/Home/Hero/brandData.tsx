3b38fea8b8ee06d782faf5e3d822ed60ac1666ca
枭格科技 <348310633@qq.com>
2024-11-07 22:04:15 +0800

    短剧分销cps
    短剧分销cps

--- src/components/Home/Hero/brandData.tsx
+++ src/components/Home/Hero/brandData.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: gzyxds 348310633@qq.com
  * @Date: 2024-10-15 14:50:41
- * @LastEditors: gzyxds 348310633@qq.com
- * @LastEditTime: 2024-11-07 21:20:54
+ * @LastEditors: 枭格科技 348310633@qq.com
+ * @LastEditTime: 2024-11-07 21:59:33
  * @FilePath: \dspcps\src\components\Home\Hero\brandData.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,7 +19,7 @@ const brandData: Brand[] = [
 	{
 		id: 1 + i,
 		name: "NextJSTemplates",
-		link: "https://nextjstemplates.com",
+		link: "https://play.urlnet.cn",
 		image: (
 			<img src="/images/icon/抖音.svg" alt="NextJSTemplates" width="101" height="28" />
 		),
@@ -27,7 +27,7 @@ const brandData: Brand[] = [
 	{
 		id: 1 + i,
 		name: "FormBold",
-		link: "https://formbold.com",
+		link: "https://play.urlnet.cn",
 		image: (
 			<img src="/images/icon/快手.svg" alt="FormBold" width="153" height="28" />
 		),
@@ -35,7 +35,7 @@ const brandData: Brand[] = [
 	{
 		id: 1 + i,
 		name: "Lineicons",
-		link: "https://lineicons.com",
+		link: "https://play.urlnet.cn",
 		image: (
 			<img src="/images/icon/视频号.svg" alt="Lineicons" width="173" height="28" />
 		),
@@ -43,23 +43,23 @@ const brandData: Brand[] = [
 	{
 		id: 1 + i,
 		name: "Uideck",
-		link: "https://uideck.com",
+		link: "https://play.urlnet.cn",
 		image: (
-			<img src="/images/icon/企业微信.svg" alt="Uideck" width="119" height="28" />
+			<img src="/images/icon/小红书.svg" alt="Uideck" width="119" height="28" />
 		),
 	},
 	{
 		id: 1 + i,
 		name: "Graygrids",
-		link: "https://graygrids.com",
+		link: "https://play.urlnet.cn",
 		image: (
-			<img src="/images/icon/小红书.svg" alt="Graygrids" width="134" height="28" />
+			<img src="/images/icon/企业微信.svg" alt="Graygrids" width="134" height="28" />
 		),
 	},
 	{
 		id: 1 + i,
 		name: "SaaSBold",
-		link: "https://SaaSBold.com",
+		link: "https://play.urlnet.cn",
 		image: (
 			<img src="/images/icon/tiktok.svg" alt="SaaSBold" width="154" height="28" />
 		),
